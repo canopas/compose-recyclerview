be38384536faa4aca3051f28df607a6e3ec764b4
cp-megh <megh.l@canopas.com>
2024-01-01 16:34:58 +0530

    Add github workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,9 @@ plugins {
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.android.library") version "8.1.2" apply false
+    id ("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+}
+
+apply{
+    from("${rootDir}/scripts/publish-root.gradle")
 }
